d0729a7dd90bad018338296da444f2f736b31d92
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-11 10:17:07 +0100

    Initial app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route } from "react-router-dom";
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Nav />
+      <div className="body">
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </>
   );
 }
 
