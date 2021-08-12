3f031d06e0241a0402e12b2bb88b3bfd16db9c40
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-12 09:50:00 +0100

    Display User profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Auth from '../../Auth';
 
 export default function Profile(props: { auth: Auth; }) {
+  const [profile, setProfile] = useState<{ nickname: string; picture: string; }>();
+  const [error, setError] = useState("");
+
+  const setState = (profile: any, error: string) => {
+    setProfile(profile);
+    setError(error);
+  };
+
+  const loadUserProfile = () => {
+    props.auth.getProfile((profile, error) => setState(profile, error));
+  };
+
+
+  useEffect(() => {
+    loadUserProfile();
+
+    return () => {
+    };
+  });
+
+  if (!profile) {
+    return <h1>
+      Loading...
+    </h1>;
+  }
+
   return (
-    <h1>
-      Profile
-    </h1>
+    <>
+      <h1>
+        Profile
+      </h1>
+
+      <p>
+        {profile.nickname}
+      </p>
+      <img style={{ maxWidth: 50, maxHeight: 50 }}
+        src={profile.picture}
+        alt="profile pic"
+      />
+      <pre>{JSON.stringify(profile, null, 2)}</pre>
+    </>
   );
 }
