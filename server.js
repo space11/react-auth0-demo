4f2b4841e6ee3f3fceeed43f43410f75210a371c
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-12 10:14:49 +0100

    Fix server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
+
 
 const app = express();
 
-app.get('/public', (req, res) => {
-  res.json({ message: 'Hello from a public API!' });
+app.get("/public", function (req, res) {
+  res.json({
+    message: "Hello from a public API!"
+  });
 });
 
-app.listen(30001, () => {
-  console.log(`API server listening on ${process.env.REACT_APP_API_URL}`);
-});
+
+app.listen(3001);
+console.log("API server listening on " + process.env.REACT_APP_AUTH0_AUDIENCE);
