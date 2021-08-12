943346a7f23b9f7de600083d54e5acfef2c89466
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-12 10:05:30 +0100

    Create API with NODE and Express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/public', (req, res) => {
+  res.json({ message: 'Hello from a public API!' });
+});
+
+app.listen(30001, () => {
+  console.log(`API server listening on ${process.env.REACT_APP_API_URL}`);
+});
