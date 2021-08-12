d2e5988ccbec77827fd6b1a50d1da16ea0f883b0
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-12 10:54:57 +0100

    Hide private link when not logged in

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav(props: { auth: Auth; }) {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/public">Public</Link></li>
-        <li><Link to="/private">Private</Link></li>
+        {isAuthenticated() && <li><Link to="/private">Private</Link></li>}
         <li>
           <button onClick={isAuthenticated() ? logout : login}>{isAuthenticated() ? "Log Out" : "Log In"}</button>
         </li>
