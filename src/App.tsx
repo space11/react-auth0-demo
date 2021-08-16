f556515b5e8d82c8c763575f86f6848e5a441eca
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-16 11:38:08 +0100

    Change to functiona component

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,49 @@
-import { History } from 'history';
-import React, { Component } from 'react';
-import { Redirect, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { Redirect, Route, useHistory } from "react-router-dom";
 import Auth from './Auth/Auth';
+import Callback from './Components/Callback';
+import Courses from './Components/Courses';
 import Home from './Components/Home';
 import Nav from './Components/Nav';
-import Profile from './Components/Profile';
-import Callback from './Components/Callback';
-import Public from './Components/Public';
 import Private from './Components/Private';
-import Courses from './Components/Courses';
+import Profile from './Components/Profile';
+import Public from './Components/Public';
 
-interface AppProps {
-  history: History;
-}
-export default class App extends Component<AppProps> {
-  auth: Auth;
-  /**
-   *
-   */
-  constructor(props: AppProps) {
-    super(props);
-    this.auth = new Auth(this.props.history);
-  }
-  render() {
-    return (
-      <>
-        <Nav auth={this.auth} />
-        <div className="body">
-          <Route path="/" exact render={(props) => <Home auth={this.auth} {...props} />} />
-          <Route path="/callback" render={(props) => <Callback auth={this.auth} {...props} />} />
-          <Route path="/profile"
-            render={
-              props =>
-                this.auth.isAuthenticated() ?
-                  <Profile auth={this.auth} {...props} />
-                  : <Redirect to="/" />
-            } />
-          <Route path="/public" component={Public} />
-          <Route path="/private"
-            render={
-              props =>
-                this.auth.isAuthenticated() ?
-                  <Private auth={this.auth} {...props} />
-                  : <Redirect to="/" />
-            } />
-          <Route path="/courses"
-            render={
-              props =>
-                this.auth.isAuthenticated() && this.auth.hasUserScopes(['read:courses']) ?
-                  <Courses auth={this.auth} {...props} />
-                  : <Redirect to="/" />
-            } />
-        </div>
-      </>
-    );
-  }
-}
+const App = () => {
+  const history = useHistory();
+  const [auth] = useState(() => new Auth(history));
+
+  return (
+    <>
+      <Nav auth={auth} />
+      <div className="body">
+        <Route path="/" exact render={(props) => <Home auth={auth} {...props} />} />
+        <Route path="/callback" render={(props) => <Callback auth={auth} {...props} />} />
+        <Route path="/profile"
+          render={
+            props =>
+              auth.isAuthenticated() ?
+                <Profile auth={auth} {...props} />
+                : <Redirect to="/" />
+          } />
+        <Route path="/public" component={Public} />
+        <Route path="/private"
+          render={
+            props =>
+              auth.isAuthenticated() ?
+                <Private auth={auth} {...props} />
+                : <Redirect to="/" />
+          } />
+        <Route path="/courses"
+          render={
+            props =>
+              auth.isAuthenticated() && auth.hasUserScopes(['read:courses']) ?
+                <Courses auth={auth} {...props} />
+                : <Redirect to="/" />
+          } />
+      </div>
+    </>
+  );
+};
+
+export default App;
