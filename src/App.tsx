38a31dcc3977b038a118833b3854d750906ae485
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-08-16 15:57:00 +0100

    Use PrivateRoute component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Route, useHistory } from "react-router-dom";
-import Auth from './Auth/Auth';
-import Callback from './Components/Callback';
-import Courses from './Components/Courses';
-import Home from './Components/Home';
-import Nav from './Components/Nav';
-import Private from './Components/Private';
-import PrivateRoute from './Components/PrivateRoute';
-import Profile from './Components/Profile';
-import Public from './Components/Public';
-import AuthContext from './Context/AuthContext';
+import Auth from "./Auth/Auth";
+import Callback from "./Components/Callback";
+import Courses from "./Components/Courses";
+import Home from "./Components/Home";
+import Nav from "./Components/Nav";
+import Private from "./Components/Private";
+import PrivateRoute from "./Components/PrivateRoute";
+import Profile from "./Components/Profile";
+import Public from "./Components/Public";
+import PublicRoute from "./Components/PublicRoute";
+import AuthContext from "./Context/AuthContext";
 
 const App = () => {
   const history = useHistory();
@@ -19,11 +20,15 @@ const App = () => {
     <AuthContext.Provider value={auth}>
       <Nav auth={auth} />
       <div className="body">
-        <Route path="/" exact render={(props) => <Home auth={auth} {...props} />} />
-        <Route path="/callback" render={(props) => <Callback auth={auth} {...props} />} />
+        <PublicRoute path="/" exact component={Home} />
+        <PublicRoute path="/callback" component={Callback} />
         <PrivateRoute path="/profile" component={Profile} />
         <PrivateRoute path="/private" component={Private} />
-        <PrivateRoute path="/courses" component={Courses} scopes={['read:courses']} />
+        <PrivateRoute
+          path="/courses"
+          component={Courses}
+          scopes={["read:courses"]}
+        />
         <Route path="/public" component={Public} />
       </div>
     </AuthContext.Provider>
